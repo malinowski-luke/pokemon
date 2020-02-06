2bdf7363e26e265c15671c7897ea9c3a212d2947
Lukasz Malinowski <lukaszmalinowski@Lukaszs-MacBook-Pro.local>
2020-02-06 14:39:30 -0700

    finishedbackend

--- server/grassController/grassController.js
+++ server/grassController/grassController.js
@@ -1,3 +1,30 @@
+const axios = require('axios')
+
 module.exports = {
-  getWildPokemon: (req, res) => {}
+  getWildPokemon: (req, res) => {
+    const pokemonArray = []
+    const rand1 = Math.ceil(Math.random() * 151)
+    const rand2 = Math.ceil(Math.random() * 151)
+    const rand3 = Math.ceil(Math.random() * 151)
+    // res response not to confuse res with axios response
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${rand1}`)
+      .then(response => {
+        pokemonArray.push(response.data)
+      })
+      .catch(err => console.log(`failed to get rand1 pokemon ${err}`))
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${rand2}`)
+      .then(response => {
+        pokemonArray.push(response.data)
+      })
+      .catch(err => console.log(`failed to get rand2 pokemon ${err}`))
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${rand3}`)
+      .then(response => {
+        pokemonArray.push(response.data)
+        res.status(200).send([pokemonArray])
+      })
+      .catch(err => console.log(`failed to get rand3 pokemon ${err}`))
+  }
 }
--- server/pokemonController/pokemonController.js
+++ server/pokemonController/pokemonController.js
@@ -5,7 +5,24 @@ module.exports = {
   getCaughtPokemon: (req, res) => {
     res.status(200).send(caughtPokemon)
   },
-  catchPokemon: (req, res) => {},
-  editPokemonName: (req, res) => {},
-  releasePokemon: (req, res) => {}
+  catchPokemon: (req, res) => {
+    const { pokemon } = req.body
+    pokemon.id = id
+    id++
+    caughtPokemon.push(pokemon)
+    res.status(200).send(caughtPokemon)
+  },
+  editPokemonName: (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+    let index = caughtPokemon.findIndex(elm => elm.id === +id)
+    caughtPokemon[index].name = name
+    res.status(200).send(caughtPokemon)
+  },
+  releasePokemon: (req, res) => {
+    const { id } = req.params
+    let index = caughtPokemon.findIndex(elm => elm.id === +id)
+    caughtPokemon.splice(index, 1)
+    res.status(200).send(caughtPokemon)
+  }
 }
